ca20e59f07844e34a546290c79c2f066b69daa18
mantodgg <252228913+mantodgg@users.noreply.github.com>
2026-08-20 23:17:07 -0300

    taller1 sin terminar

---
+++ taller1/ejercicios/ej2/sumador_completo.sv
@@ -0,0 +1,26 @@
+module sumador_completo (
+    input  logic a,
+    input  logic b,
+    input  logic cin,
+    output logic sum,
+    output logic cout
+);
+  logic d1, c1, c2;
+  // instanciar dos sumador_simple y armar cout
+  sumador_simple ss1 (
+    .a(a),
+    .b(b),
+    .sum(d1),
+    .cout(c1)
+  );
+
+  sumador_simple ss2 (
+    .a(d1),
+    .b(cin),
+    .sum(sum),
+    .cout(c2)
+  );
+
+  assign cout = c1 | c2;
+
+endmodule
---
+++ taller1/ejercicios/ej5/restador_completo.sv
@@ -0,0 +1,25 @@
+module restador_completo (
+    input  logic a,
+    input  logic b,
+    input  logic bin,
+    output logic diff,
+    output logic bout
+);
+  logic d1, b1, b2;
+  // instanciar dos restador_simple y armar bout
+  restador_simple rs1 (
+    .a(a),
+    .b(b),
+    .diff(d1),
+    .bout(b1)
+  );
+
+  restador_simple rs2 (
+    .a(d1),
+    .b(bin),
+    .diff(diff),
+    .bout(b2)
+  );
+
+  assign bout = b1 | b2;
+endmodule
