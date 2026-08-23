b7b87813cbb82e3a8e95ab829fb1a20839571374
mantodgg <252228913+mantodgg@users.noreply.github.com>
2026-08-23 03:17:59 +0000

    ejercicio 8

---
+++ taller1/ejercicios/ej8/restador_simple.sv
@@ -0,0 +1,10 @@
+module restador_simple (
+    input  logic a,
+    input  logic b,
+    output logic diff,
+    output logic bout
+);
+  // completar los assign
+    assign diff = a ^ b;
+    assign bout = ~a & b;
+endmodule
--- taller1/ejercicios/ej8/sumador_restador_4b.sv
+++ taller1/ejercicios/ej8/sumador_restador_4b.sv
@@ -7,5 +7,23 @@ module sumador_restador_4b (
 );
   logic [3:0] sum, diff;
   logic       add_cout, bout;
-  // instanciar sumador_4b y restador_4b; elegir con ctrl
+  sumador_4b sumador (
+    .a(a),
+    .b(b),
+    .cin(0),
+    .sum(sum),
+    .cout(add_cout)
+  );
+
+  restador_4b restador (
+    .a(a),
+    .b(b),
+    .bin(0),
+    .diff(diff),
+    .bout(bout)
+  );
+
+  assign y = ctrl ? diff : sum;
+  assign cout = ctrl ? bout : add_cout;
 endmodule
+
