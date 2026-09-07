2e01c92e067f3baff02d12326ae1f7a9320a7340
mantodgg <252228913+mantodgg@users.noreply.github.com>
2026-09-07 21:18:58 +0000

    ej1 y 2

--- taller3/taller/ejercicios/ej1/and_4b.sv
+++ taller3/taller/ejercicios/ej1/and_4b.sv
@@ -3,5 +3,5 @@ module compuerta_and_4b (
     input  logic [3:0] b,
     output logic [3:0] result
 );
-  // Completar: AND bit a bit entre a y b.
+  assign result = a & b;
 endmodule
--- taller3/taller/ejercicios/ej2/or_4b.sv
+++ taller3/taller/ejercicios/ej2/or_4b.sv
@@ -3,5 +3,5 @@ module compuerta_or_4b (
     input  logic [3:0] b,
     output logic [3:0] result
 );
-  // Completar: OR bit a bit entre a y b.
+  assign result = a | b;
 endmodule
