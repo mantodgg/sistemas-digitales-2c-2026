401e16134bd60620042c6d790f5baefbd9df9717
mantodgg <252228913+mantodgg@users.noreply.github.com>
2026-09-11 03:27:36 +0000

    ejercicio 7 terminado

--- taller4/taller/ejercicios/ej7/shifter_4b.sv
+++ taller4/taller/ejercicios/ej7/shifter_4b.sv
@@ -8,22 +8,32 @@ module shifter_4b (
   // Reutilizar negativo_4b y zero_4b sobre resultado.
   // En modo lógico, ambos flags deben valer 0.
 
+  logic zero_1, negativo_1;
+  logic zero_0, negativo_0;
+
+  assign zero_0 = 1'b0;
+  assign negativo_0 = 1'b0;
+
   always_comb begin
     if (aritmetico) begin
-      resultado = {1'b1,dato[3:1]}; 
+      resultado = {dato[3],dato[3:1]}; 
+      zero =  zero_1;
+      negativo = negativo_1;
     end else begin
       resultado = {1'b0,dato[3:1]};
+      zero = zero_0;
+      negativo = negativo_0;
     end
   end
 
   negativo_4b negativo_4b (
     .dato    (resultado),
-    .negativo(negativo)
+    .negativo(negativo_1)
   );
 
   zero_4b zero_4b (
     .dato(resultado),
-    .zero(zero)
+    .zero(zero_1)
   );
-  
+
 endmodule
