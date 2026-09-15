ac5a1a9bbea80499121d6a11fa4068e885294044
mantodgg <252228913+mantodgg@users.noreply.github.com>
2026-09-14 21:28:10 -0300

    ej8

---
+++ guia1/ej8.sv
@@ -0,0 +1,19 @@
+// lo hice para 4 bits aunque el enunciado pide que hay que hacerlo para N bits. no importa, la implementacion es parecida.
+
+module inversor_controlado (
+    input logic [3:0] in,
+    input logic invert,
+    output logic [3:0] out
+);
+    always_comb begin
+        if (invert) begin
+            out[0] = ~in[0];
+            out[1] = ~in[1];
+            out[2] = ~in[2];
+            out[3] = ~in[3];
+        end
+        else begin
+            out = in;
+        end
+    end
+endmodule
