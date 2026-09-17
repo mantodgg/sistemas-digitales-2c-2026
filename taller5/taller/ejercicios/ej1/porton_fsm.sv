46f90e06d6e73cb53c4a3b6cf1eba93a82edcd85
mantodgg <252228913+mantodgg@users.noreply.github.com>
2026-09-17 17:58:21 +0000

    taller 5

---
+++ taller5/taller/ejercicios/ej1/porton_fsm.sv
@@ -0,0 +1,12 @@
+module porton_fsm (
+    input  logic clk,
+    input  logic rst,
+    input  logic button,
+    output logic cerrado,
+    output logic abriendo,
+    output logic abierto,
+    output logic cerrando
+);
+  // COMPLETAR: FSM Moore con estados CERRADO, ABRIENDO, ABIERTO y CERRANDO.
+endmodule
+
