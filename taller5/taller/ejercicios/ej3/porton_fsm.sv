a8750c78b14c532faf1c836cca1972f993848bac
mantodgg <252228913+mantodgg@users.noreply.github.com>
2026-09-18 03:01:22 +0000

    ejercicio 3 terminado ????

--- taller5/taller/ejercicios/ej3/porton_fsm.sv
+++ taller5/taller/ejercicios/ej3/porton_fsm.sv
@@ -6,4 +6,55 @@ module porton_fsm_smart (
 );
   // COMPLETAR: un click inicia; durante el movimiento pausa; desde la pausa
   // otro click inicia el movimiento en la dirección opuesta.
+  typedef enum logic [2:0] {  
+      S0, //cerrado
+      S1, //abriendo
+      S2, //abierto
+      S3, //cerrando
+      S4, //pausa subiendo
+      S5 //pausa bajando
+    } state_t;
+
+    state_t current_state, next_state;
+
+    always_ff @(posedge clk or posedge rst) begin
+      if (rst)
+        current_state <= S0;
+      else
+        current_state <= next_state;
+    end
+
+    always_comb begin
+      next_state = current_state;
+
+      unique case (current_state)
+        S0: if (button) next_state = S1;
+        S1: begin
+            if (posicion == 2'b11) next_state = S2;
+            if (button) next_state = S4;
+        end 
+        S2: if (button) next_state = S3;
+        S3: begin
+            if (posicion == 2'b00) next_state = S0;
+            if (button) next_state = S5;
+        end
+        S4: begin
+            if (posicion == 2'b11) next_state = S2;
+            if (posicion == 2'b00) next_state = S0;
+            if (button) next_state = S3;
+        end
+        S5: begin
+            if (posicion == 2'b00) next_state = S0;
+            if (posicion == 2'b11) next_state = S3;
+            if (button) next_state = S1;
+        end
+        default: next_state = S0;
+      endcase
+    end
+
+    assign cerrado = current_state == S0;
+    assign abrir = current_state == S1;
+    assign abierto = current_state == S2;
+    assign cerrar = current_state == S3;
+    assign pausa = current_state == S4 | current_state == S5;
 endmodule
